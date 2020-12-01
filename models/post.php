4426151f34418fa586e313889b41b2783ea3e8e2
nicollaslopes <nicollaslopesn@gmail.com>
2020-12-01 13:45:41 -0300

    adicao do metodo create

--- models/post.php
+++ models/post.php
@@ -75,9 +75,37 @@
             $this->author = $row['author'];
             $this->category_id = $row['category_id'];
             $this->category_name = $row['category_name'];
-            
-            
+        }
 
-                
+        public function create(){
+
+            $query = '
+                    INSERT INTO ' . $this->table . '
+                    SET 
+                        title = :title,    
+                        body = :body,    
+                        author = :author,    
+                        category_id = :category_id,        
+                    ';
+
+            $stmt = $this->con->prepare($query);
+
+            $this->title = htmlspecialchars(strip_tags($this->title));
+            $this->author = htmlspecialchars(strip_tags($this->author));
+            $this->body = htmlspecialchars(strip_tags($this->body));
+            $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+            $stmt->bindParam(':title', $this->title);
+            $stmt->bindParam(':author', $this->author);
+            $stmt->bindParam(':body', $this->body);
+            $stmt->bindParam(':category_id', $this->category_id);
+            
+            if($stmt->execute()){
+                return true;
+            } else {
+                return false;
+            }
+
+        
         }
     }

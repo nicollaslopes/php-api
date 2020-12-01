421f7563f79b96a57dc7eb7899502d5c057df730
nicollaslopes <nicollaslopesn@gmail.com>
2020-12-01 08:41:44 -0300

    adicao do metodo readSingle

--- models/post.php
+++ models/post.php
@@ -1,23 +1,23 @@
 <?php
 
-    class Post {
+    class Post{
 
         private $con;
         private $table = 'posts';
-        
+
         public $id;
         public $category_id;
-        public $category_name;
         public $title;
-        public $author;
         public $body;
+        public $author;
+        public $category_name;
         public $created_at;
-        
+
         public function __construct($db)
         {
             $this->con = $db;
         }
-        
+
         public function read(){
 
             $query = 'SELECT
@@ -28,17 +28,56 @@
                         p.body,
                         p.author,
                         p.created_at
-                    FROM
+                    FROM 
                         ' . $this->table . ' p
                     LEFT JOIN
                         categories c ON p.category_id = c.id
-                        ';
-            
+                    ORDER BY
+                        p.created_at DESC
+                    ';
+
             $stmt = $this->con->prepare($query);
 
             $stmt->execute();
 
-            return $stmt;
+            return $stmt;   
         }
-    
+
+        public function readSingle(){
+
+            $query = 'SELECT
+                        c.name as category_name,
+                        p.id,
+                        p.category_id,
+                        p.title,
+                        p.body,
+                        p.author,
+                        p.created_at
+                    FROM 
+                        ' . $this->table . ' p
+                    LEFT JOIN
+                        categories c ON p.category_id = c.id
+                    WHERE
+                        p.id = ?
+                    LIMIT 0,1 
+                    ';
+            
+            $stmt = $this->con->prepare($query);
+
+            $stmt->bindParam(1, $this->id); // pegando o primeiro parâmetro passado na query > "?"
+
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $this->title = $row['title'];
+            $this->body = $row['body'];
+            $this->author = $row['author'];
+            $this->category_id = $row['category_id'];
+            $this->category_name = $row['category_name'];
+            
+            
+
+                
+        }
     }
